a73ac73d206e27add091c629ebf94b4c8d3aa8e4
xigua_akako <xiguaakako@gmail.com>
2022-12-20 09:58:02 +0800

    hack3

--- hack3/frontend/src/graphql/mutations.js
+++ hack3/frontend/src/graphql/mutations.js
@@ -2,6 +2,18 @@ import { gql } from '@apollo/client';
 
 // TODO 3.1 Mutation - Update item
 
+export const CREATE_ITEM_MUTATION = gql`
+  mutation CreateItem($input: CreateItemInput!) {
+    createItem($input: $input) {
+      id
+      name
+      amount
+      category
+      date
+      description
+    }
+  }
+`
 // TODO 3.1 End
 
 export const UPDATE_ITEM_MUTATION = gql`
--- hack3/frontend/src/graphql/queries.js
+++ hack3/frontend/src/graphql/queries.js
@@ -1,7 +1,6 @@
 import { gql } from '@apollo/client';
 
-export const GET_ITEMS_QUERY = gql
-`
+export const GET_ITEMS_QUERY = gql`
 query GET_ITEMS_QUERY {
   items{
     id
