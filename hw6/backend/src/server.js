825a70a67db2ac2e714c254c4dfff38786d0630e
xigua_akako <xiguaakako@gmail.com>
2022-12-25 01:07:58 +0800

    alpha01

--- hw6/backend/src/server.js
+++ hw6/backend/src/server.js
@@ -1,17 +1,31 @@
+import path from 'path';
 import express from 'express';
 import cors from 'cors';
 import db from './db';
 import routes from './routes';
 
 const app = express();
-app.use(cors());
+if (process.env.NODE_ENV === "development")
+    app.use(cors());
+
 app.use(express.json());
 app.use('/', routes);
 
-const port = process.env.PORT || 4000;
-    app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     res.send('Hello, World!');
-});
+});*/
+
+
+if (process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, "../frontend", "build")));
+    app.get("/*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+    });
+}
+const port = process.env.PORT || 4000;
+
+
 app.listen(port, () =>
     console.log(`Example app listening on port ${port}!`),
 );
--- hw6/frontend/src/api.js
+++ hw6/frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_ROOT = process.env.NODE_ENV === "production" ? "/" : "http://localhost:4000/";
+
 const instance = axios.create({
-  baseURL: `http://localhost:4000/`,
+  baseURL: API_ROOT,
 });
 
 export default instance;
